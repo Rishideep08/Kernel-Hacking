141276842f8519872426d22c840654dc3d22bc4d
Rishideep08 <rallabandirishideepreddy@gmail.com>
2022-12-28 04:16:23 -0600

    Thread and Locking lib

---
+++ Xv6 thread and locking library/xv6-public/sysproc.c
@@ -0,0 +1,144 @@
+#include "types.h"
+#include "x86.h"
+#include "defs.h"
+#include "date.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+
+int
+sys_fork(void)
+{
+  return fork();
+}
+
+int
+sys_exit(void)
+{
+  exit();
+  return 0;  // not reached
+}
+
+int
+sys_wait(void)
+{
+  return wait();
+}
+
+int
+sys_kill(void)
+{
+  int pid;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+  return kill(pid);
+}
+
+int
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+int
+sys_sbrk(void)
+{
+  int addr;
+  int n;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+int
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(myproc()->killed){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+int
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
+
+
+int
+sys_clone(void)
+{
+  // actual clone call prototype : 
+  // clone(int (*fun)(void *), void *child_stack), int flags, void *args)
+    
+  int (*func)(void *);
+  char *child_stack, *args;
+  int flags;
+
+  // read the function pointer
+  if(argptr(0, (char **)&func, 0) == -1) {
+      return -1;
+  }
+  
+  // read the child stack address
+  if(argint(1, (int *)&child_stack) == -1) {
+      return -1;
+  }
+  
+  // read the flags 
+  if(argint(2, &flags) == -1) {
+      return -1;
+  }
+  
+  // read the address of argument 
+  if(argptr(3, &args, 0) == -1) {
+      return -1;
+  }
+  
+  // // call to actual kernel clone function 
+  // return clone(func, (void *)child_stack, flags, (void *)args);
+}
+
+int
+sys_join(void)
+{
+  cprintf("Hi in the join function\n");
+  return 0;
+}
+
+int 
+sys_thread_create(void){
+   void *stack = malloc(PGSIZE);
+   sys_clone(func, (void *)child_stack, flags, (void *)args);
+}
+
+int
+sys_thread_join(void){
+
+}
